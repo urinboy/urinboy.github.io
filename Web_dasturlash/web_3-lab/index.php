4f1230fc64bdf143460e8d380e6559ed2503c1da
Urinboy <urinboytursunboev@gmail.com>
2021-05-26 08:09:11 +0500

    update web 3

---
+++ Web_dasturlash/web_3-lab/index.php
@@ -0,0 +1,137 @@
+<?php
+$title = "Talabaning Shaxsiy Varaqasi";
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equive="X-UA-Comptible" content="ie=edge">
+        <title><?= $title; ?></title>
+        <link rel="stylesheet" href="styles.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  
+    </head>
+    <body>
+            
+<div class="container">
+    <div class="main-body">
+            
+          <!-- Breadcrumb -->
+          <nav aria-label="breadcrumb" class="main-breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="/">Asosiy</a></li>
+              <li class="breadcrumb-item active"><a href="/web_3-lab"><?= $title; ?></a></li>
+            </ol>
+          </nav>
+          <!-- /Breadcrumb -->
+    
+          <div class="row gutters-sm">
+            <div class="col-md-4 mb-3">
+              <div class="card">
+                <div class="card-body">
+                  <div class="d-flex flex-column align-items-center text-center">
+                    <img src="https://tuormedia.uz/styles/img/favicon.png" alt="Admin" class="rounded-circle" width="150">
+                    <div class="mt-3">
+                      <h4>TUORMedia.uz </h4>
+                      <p class="text-secondary mb-1">@tuormedia_uz </p>
+     <p class="text-muted font-size-sm">Saytning telegram kanaliga </p>
+                      <a href="https://t.me/joinchat/WT8o8WM2cgDhDDkK"><button class="btn btn-primary">Qo'shilish</button></a>
+                     <a href="https://t.me/tuormedia"><button class="btn btn-outline-primary">Xabar yo'llash</button></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-8">
+              <div class="card mb-3">
+                <div class="card-body">
+<h3 class="text-center"><?= $title; ?></h3>
+                  <div class="container">
+<div class="alert alert-danger text-center">*  yulduzchali kataklar to'ldirilishi shart!</div>
+<table>
+                <tr>
+                    <td style="width:130px;boprder:1px solid #ccc;padding: 3px;">
+                       <img src="talaba.jpg" class="image" alt="Talaba rasmi">
+                    </td>
+                    <td colspan="2" width="630">
+                        <h4 style="margin-top:-50px;">TALABANING SHAXSIY VARAQASI</h4>
+                        <p style="margin-left:60px;padding-left: 40px;">
+                            Yig`ma jild va reyting daftarchasi № <b>  1061911</b><br>
+                            Mutaxassislik "<b>Kompyuter injinitingi: AT-Servis</b>"<br>
+                            F.I.Sh: <b>  TURSUNBOYEV  O'RINBOY   ODILOVICH</b>
+                            
+                        </p>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td><span class="num">1.</span></td>
+                    <td>Tug`ilgan yili, oyi, kuni</td>
+                    <td>1996.05.10</td>
+                </tr>
+                <tr>
+                    <td><span class="num">2.</span></td>
+                    <td>Tug`ilgan joyi</td>
+                    <td>Qoraqalpog`iston Respublikasi, Xo`jayli tumani</td>
+                </tr>
+                <tr>
+                    <td><span class="num">3.</span></td>
+                    <td>Millati</td>
+                    <td>O`zbek</td>
+                </tr>
+                <tr>
+                    <td><span class="num">4.</span></td>
+                    <td>Ma'lumoti (maktab, akademik litsey, KHK yoki boshqa o`quv yurti N# nomi tugatgan yili</td>
+                    <td>Xo'jayli Pedagogika kolleji</td>
+                </tr>
+                <tr>
+                    <td><span class="num">5.</span></td>
+                    <td>O`qishga kirgunga qadar ish joyi, mansabi (agar ishlagan bo`lsa)</td>
+                    <td>Taxiatosh tibbiyot birlashmasiga qarashli Keneges qishloq vrachlik punkti, posbon.</td>
+                </tr>
+                <tr>
+                    <td><span class="num">6.</span></td>
+                    <td>Ota-ona haqida ma'lumot (F.I.Sh qayerda kim bo`lib ishlaydi, telefon raqami</td>
+                    <td>Ishnazarov Odil,<br> Taxiatosh issilik energiya montaj, mantajchi.<br>+998913729896
+                    <br>Jumanova Dilbar,<br> Taxiatosh tibbiyot birlashmasiga qarashli
+                        Keneges qishloq vrachlik punkti, xo'jalik bekasi.<br>+998913072039</td>
+                </tr>
+                <tr>
+                    <td><span class="num">7.</span></td>
+                    <td>Oilaviy axvoli (turmush o`rtog`ining F.I.Sh qayerda, kim bo`lib ishlaydi telefon raqami</td>
+                    <td>Uylanmagan</td>
+                </tr>
+                <tr>
+                    <td><span class="num">8.</span></td>
+                    <td>Pasport seriyasi, raqami, kim tomonidan, qachon berilga</td>
+                    <td>KA 061786,
+                        <br>QORAQALPOG'ISTON RESPUBLIKASI XOJAYLI SHTIIB,
+                        <br>01.09.2015</td>
+                </tr>
+                <tr>
+                    <td><span class="num">9.</span></td>
+                    <td>Ota-onasining yashash manzili</td>
+                    <td>Taxiatosh tumani Keneges OFY</td>
+                </tr>
+                <tr>
+                    <td><span class="num">10.</span></td>
+                    <td>Uy manzili, shu jumladan ijara uy, telefoni</td>
+                    <td>Taxiatosh tumani Keneges OFY,
+                    <br>Nukus shahri Amanbayev № 3
+                        <br>+998993184236</td>
+                </tr>            
+            </table>
+       
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+
+
+    </body>
+</html>
