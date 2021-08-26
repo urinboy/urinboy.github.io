65e3760cdec8ff766d71465c98f873c6843ba292
Urinboy <51972400+urinboy@users.noreply.github.com>
2021-08-26 06:11:12 +0500

    Update index and style

--- learn/index.rb
+++ learn/index.rb
@@ -1,2 +1,14 @@
-print "Salom Ruby!"
+puts "Urinboy"
+puts " is cool"
+print "Salom Ruby! \n"
 print "Bugun birinchi Ruby Dasturini yaratmoqdaman"
+print " is cool \n"
+puts "   /|"
+puts "  / |"
+puts " /  |"
+puts "/___|"
+print 3+9
+print 3-9
+print 3*9
+print 3/9
+# Bundan keyingilar shunday davom etadi.
