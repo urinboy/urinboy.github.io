5801393fea121071260582013a702c7e2edcec3f
Urinboy <51972400+urinboy@users.noreply.github.com>
2022-01-17 01:11:17 +0500

    update project 4 and js lesson 7

--- WebForte/Lesson_js/lesson_js_7/misol_4/index.js
+++ WebForte/Lesson_js/lesson_js_7/misol_4/index.js
@@ -1,22 +1,38 @@
-/* ** Misol #3** */
+/* ** Misol #4** */
 
-function taqqoslash() {
-    let str = document.querySelector('#str').value;
-    let natija = document.querySelector("#natija");
+// Kiritish uchun
+function kiritish(num){
+    document.form.display_calc.value = document.form.display_calc.value + num; 
+}
 
-
-
-    natija.innerHTML = teskariMatn(str);
-    //consolga natijani chiqarish
-    console.log(teskariMatn(str));
-
-
-    // Matnni teskskari yozuvchi funksiya
-    function teskariMatn(str) {
-        if (str === "")
-            return "";
-
-        else
-            return teskariMatn(str.substr(1)) + str.charAt(0);
+// Natijani ekranga chiqarish
+function natija() {
+    let exp = document.form.display_calc.value;
+    if (exp) {
+        document.form.display_calc.value = eval(exp);
     }
+}
+
+// Tozalash uchun
+
+function tozalash() {
+    document.form.display_calc.value = "";    
+}
+
+// Bittalab o'chirish uchun
+function ochirish() {
+    let exp = document.form.display_calc.value;
+    document.form.display_calc.value = exp.substring(0,exp.length - 1);        
+}
+
+// Ildiz chiqarish
+function ildiz() {
+    let exp = document.form.display_calc.value;
+    document.form.display_calc.value = Math.sqrt(exp);
+}
+
+// Kvadrat chiqarish
+function kvadrat() {
+    let exp = document.form.display_calc.value;
+    document.form.display_calc.value = Math.pow(exp, 2);
 }
