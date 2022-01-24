d80b6af7225922a3a00fab3adc403cce7fc7ee52
Urinboy <51972400+urinboy@users.noreply.github.com>
2022-01-24 13:17:39 +0500

    js9 updated

--- WebForte/Lesson_js/lesson_js_8/misol_2/index.js
+++ WebForte/Lesson_js/lesson_js_8/misol_2/index.js
@@ -1,20 +1,53 @@
-/* ** Misol #2** */
+/* ** Misol #1** */
 
-function taqqoslash() {
-    let a = parseInt(document.querySelector('#a').value);
-    let b = parseInt(document.querySelector('#b').value);
-    let natija = document.querySelector("#natija");
-    let text = "";
-    //eng kattasini aniqlovchi funksiya
-    MAX(a, b);
 
-    function MAX(a, b) {
-        if (a > b) {
-            text = `a = ${a}`;
-        } else {
-            text = `b = ${b}`;
-        }
-        natija.innerHTML = text;
-    }
+let slider = document.querySelector("#myRange");
 
+let matn = document.querySelector(".font");
+matn.style.fontSize= slider.value;
+
+let output = document.querySelector("#fontSize");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+    matn.style.fontSize = this.value;
+    output.innerHTML = this.value;
 }
+
+
+
+// function hisoblash() {
+//     let a = parseInt(document.querySelector('#a').value);
+//     let b = parseInt(document.querySelector('#b').value);
+//     let c = parseInt(document.querySelector('#c').value);
+//     let natija = document.querySelector("#natija");
+//     let text = "";
+//     let x1 = 0;
+//     let x2 = 0;
+
+//     KV(a, b, c);
+
+//     function D(a, b, c) {
+//         let Disc = Math.pow(b, 2) - 4 * a * c;
+//         return Disc;
+//     }
+
+//     function KV() {
+//         if (D(a, b, c) < 0) {
+//             text = "Ildizi mavjud emas! <br>D = " + D(a, b, c);
+//         } else if (D(a, b, c) === 0) {
+//             x1 = (-b + Math.sqrt(D(a, b, c))) / (2 * a);
+//             x2 = (-b - Math.sqrt(D(a, b, c))) / (2 * a);
+//             if (x1 === x2)
+//                 text = `x = ${x1},  D = ${D(a,b,c)}`;
+//             else
+//                 text = `x1 = ${x1}, x2 = ${x2}, <br>D = ${D(a,b,c)}`;
+//         } else {
+//             x1 = (-b + Math.sqrt(D(a, b, c))) / (2 * a);
+//             x2 = (-b - Math.sqrt(D(a, b, c))) / (2 * a);
+//             text = `x1 = ${x1}, x2 = ${x2}, <br>D = ${D(a,b,c)}`;
+//         }
+//         natija.innerHTML = text;
+//     }
+
+// }
--- WebForte/Lesson_js/lesson_js_8/misol_3/index.js
+++ WebForte/Lesson_js/lesson_js_8/misol_3/index.js
@@ -1,22 +1,53 @@
-/* ** Misol #3** */
-
-function taqqoslash() {
-    let str = document.querySelector('#str').value;
-    let natija = document.querySelector("#natija");
+/* ** Misol #1** */
 
 
+let slider = document.querySelector("#myRange");
 
-    natija.innerHTML = teskariMatn(str);
-    //consolga natijani chiqarish
-    console.log(teskariMatn(str));
+let matn = document.querySelector(".font");
+matn.style.fontSize= slider.value;
 
+let output = document.querySelector("#fontSize");
+output.innerHTML = slider.value;
 
-    // Matnni teskskari yozuvchi funksiya
-    function teskariMatn(str) {
-        if (str === "")
-            return "";
-
-        else
-            return teskariMatn(str.substr(1)) + str.charAt(0);
-    }
+slider.oninput = function() {
+    matn.style.fontSize = this.value;
+    output.innerHTML = this.value;
 }
+
+
+
+// function hisoblash() {
+//     let a = parseInt(document.querySelector('#a').value);
+//     let b = parseInt(document.querySelector('#b').value);
+//     let c = parseInt(document.querySelector('#c').value);
+//     let natija = document.querySelector("#natija");
+//     let text = "";
+//     let x1 = 0;
+//     let x2 = 0;
+
+//     KV(a, b, c);
+
+//     function D(a, b, c) {
+//         let Disc = Math.pow(b, 2) - 4 * a * c;
+//         return Disc;
+//     }
+
+//     function KV() {
+//         if (D(a, b, c) < 0) {
+//             text = "Ildizi mavjud emas! <br>D = " + D(a, b, c);
+//         } else if (D(a, b, c) === 0) {
+//             x1 = (-b + Math.sqrt(D(a, b, c))) / (2 * a);
+//             x2 = (-b - Math.sqrt(D(a, b, c))) / (2 * a);
+//             if (x1 === x2)
+//                 text = `x = ${x1},  D = ${D(a,b,c)}`;
+//             else
+//                 text = `x1 = ${x1}, x2 = ${x2}, <br>D = ${D(a,b,c)}`;
+//         } else {
+//             x1 = (-b + Math.sqrt(D(a, b, c))) / (2 * a);
+//             x2 = (-b - Math.sqrt(D(a, b, c))) / (2 * a);
+//             text = `x1 = ${x1}, x2 = ${x2}, <br>D = ${D(a,b,c)}`;
+//         }
+//         natija.innerHTML = text;
+//     }
+
+// }
