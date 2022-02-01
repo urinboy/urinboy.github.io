3ce9ab44d6e08ee076ba3817b429d4ec4cafbe01
Urinboy <51972400+urinboy@users.noreply.github.com>
2022-02-01 15:03:19 +0500

    resume updated

--- WebForte/Lesson_js/lesson_js_8/misol_1/index.js
+++ WebForte/Lesson_js/lesson_js_8/misol_1/index.js
@@ -1,16 +1,18 @@
 /* ** Misol #1** */
 
-let slider = document.querySelector("#myRange");
+document.querySelector("#myRange").addEventListener('input', function(e){
+    document.querySelector("#font").style.fontSize= this.value + "px";
+    document.querySelector("#fontSize").innerHTML = this.value;
+});
 
-let output = document.querySelector("#fontSize");
-output.innerHTML = slider.value;
 
-let matn = document.querySelector("#font");
-matn.style.fontSize= output.innerHTML;
+// let slider = document.querySelector("#myRange");
+// let output = document.querySelector("#fontSize");
+// output.innerHTML = slider.value;
+// let matn = document.querySelector("#font");
+// matn.style.fontSize= output.innerHTML;
     
-slider.oninput = function(e) {
-
-    output.innerHTML = this.value;
-
-    document.querySelector("#font").style.fontSize = output.innerHTML   + "px";
-}
+// slider.oninput = function(e) {
+//     output.innerHTML = this.value;
+//     document.querySelector("#font").style.fontSize = output.innerHTML   + "px";
+// }
